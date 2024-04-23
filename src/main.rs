1af2d9db297ffa2a6eea7f59a93668e3b1fe50bd
erikbohne <eriknbohne@gmail.com>
2024-04-23 18:53:30 +0200

    secret number guesser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("What is your guess?");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            },
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
 }
